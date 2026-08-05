58fb446ae78f27592e1fd4154d2a57095e98c4e7
Gerardo <haliasjohel@gmail.com>
2026-08-05 01:15:06 -0300

    Agrega fondo animado al menú principal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aventura-palabras-runtime-v10";
+const CACHE_NAME = "aventura-palabras-runtime-v11";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -12,6 +12,7 @@ const CORE_ASSETS = [
   "./assets/icons/icon-512.png",
   "./assets/images/intro/libro-intro-sin-cristal.png",
   "./assets/images/intro/cristal-icono.png",
+  "./assets/images/fondos/menu-inicio-aventura.png",
   "./assets/icons/icon-maskable-512.png",
   "./assets/images/elements/cristal-sabiduria-esmeralda.png",
   "./assets/images/personajes/versus/dragon-base.png",
