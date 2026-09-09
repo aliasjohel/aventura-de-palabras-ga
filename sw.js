ce5c0aeb8cd802e18cde8984a3fab5319105d87b
Gerardo <haliasjohel@gmail.com>
2026-09-09 20:38:32 -0300

    Ajusta encuadre horizontal y personajes de la mision de Calamo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "aventura-palabras-runtime-";
-const CACHE_NAME = `${CACHE_PREFIX}v261`;
+const CACHE_NAME = `${CACHE_PREFIX}v262`;
 const LEGACY_CACHE_NAMES = new Set([
   `${CACHE_PREFIX}v227`,
   `${CACHE_PREFIX}v226`,
